ff80fc2adf96c187066d985b1c0bd9a007faaeb3
daiwei <185098535@qq.com>
2017-08-06 19:16:27 +0800

    设置的样式 优化

--- src/components/setting/setting.vue
+++ src/components/setting/setting.vue
@@ -2,9 +2,12 @@
   <div class="setting">
   	<div class="setting-warpper">
   		<h3 class="title">设置</h3>
-  		<div class="set_bg">
+  		<div class="set_bg set_list border-1px">
   			<span class="name">背景设置</span>
-			<v-switch :options="options" @selectBgType="selectBgType"></v-switch>
+			<div class="switch_bg switch-right">
+  				<div class="title">{{getImageInfoTitle}}</div>
+				<v-switch :options="options" @selectBgType="selectBgType"></v-switch>
+			</div>
   		</div>
   	</div>
   </div>
@@ -37,6 +40,9 @@
 	computed: {
 		getImageInfo () {
 			return store.getters.getShowBingImage
+		},
+		getImageInfoTitle () {
+			return store.getters.getShowBingImage ? 'bing壁纸' : '自定义壁纸'
 		}
 	},
   	components: {
@@ -46,6 +52,7 @@
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/global.styl'
+@import '../../common/stylus/border-1px/index.styl'
 	.setting
 		position:fixed
 		top:0
@@ -71,7 +78,29 @@
 			.title
 				color:$text_color
 				margin:0
-			.set_bg
+			.set_list
 				padding:10px 0
 				color:$text_color
+				font-size:0
+			.border-1px
+				border-1px($border_bottom_color_deep, bottom)
+				.name
+					display:inline-block
+					font-size:16px
+					vertical-align:middle
+					width: calc(100% - 150px)
+				.switch-right
+					display:inline-block
+					vertical-align:middle
+					font-size:0
+					float: right
+					.title
+						display:inline-block
+						vertical-align:middle
+						font-size:14px
+						margin-right: 10px;
+						color:$text_color_active
+					.switch
+						display:inline-block
+						vertical-align:middle
 </style>
