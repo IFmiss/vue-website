a090283062983c719ad5d9e7246fdd4638cc16f5
ifmiss <185098535@qq.com>
2017-08-11 18:12:52 +0800

    获取了所有的安全色  显示效果待优化

--- src/components/common/colorpicker/colorpicker.vue
+++ src/components/common/colorpicker/colorpicker.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="colorpicker">
-    <div class="colorlist" v-for="(item, index) in color">
+    <div class="colorlist" v-for="(item, index) in getColor">
       <div class="color-detail" v-for="(detail, index) in item" :style="{backgroundColor:'#' + detail}" :data-color="'#' + detail" @click="changeBg($event)">
-        <!-- {{detail}} -->
       </div>
     </div>
   </div>
@@ -12,21 +11,21 @@ import store from './../../../store'
 export default {
   data () {
     return {
-      color: [
-          ['003366', '336699', '3366cc', '003399', '000099', '0000cc', '000066'],
-          ['006666', '006699', '0099cc', '0066cc', '0033cc', '0000ff', '3333ff', '333399'],
-          ['669999', '009999', '33cccc', '00ccff', '0099ff', '0066ff', '3366ff', '3333cc', '666699'],
-          ['339966', '00cc99', '00ffcc', '00ffff', '33ccff', '3399ff', '6699ff', '6666ff', '6600ff', '6600cc'],
-          ['339933', '00cc66', '00ff99', '66ffcc', '66ffff', '66ccff', '99ccff', '9999ff', '9966ff', '9933ff', '9900ff'],
-          ['006600', '00cc00', '00ff00', '66ff99', '99ffcc', 'ccffff', 'ccccff', 'cc99ff', 'cc66ff', 'cc33ff', 'cc00ff', '9900cc'],
-          ['003300', '009933', '33cc33', '66ff66', '99ff99', 'ccffcc', 'ffffff', 'ffccff', 'ff99ff', 'ff66ff', 'ff00ff', 'cc00cc', '660066'],
-          ['333300', '009900', '66ff33', '99ff66', 'ccff99', 'ffffcc', 'ffcccc', 'ff99cc', 'ff66cc', 'ff33cc', 'cc0099', '993399'],
-          ['336600', '669900', '99ff33', 'ccff66', 'ffff99', 'ffcc99', 'ff9999', 'ff6699', 'ff3399', 'cc3399', '990099'],
-          ['666633', '99cc00', 'ccff33', 'ffff66', 'ffcc66', 'ff9966', 'ff6666', 'ff0066', 'd60094', '993366'],
-          ['a58800', 'cccc00', 'ffff00', 'ffcc00', 'ff9933', 'ff6600', 'ff0033', 'cc0066', '660033'],
-          ['996633', 'cc9900', 'ff9900', 'cc6600', 'ff3300', 'ff0000', 'cc0000', '990033'],
-          ['663300', '996600', 'cc3300', '993300', '990000', '800000', '993333']
-      ]
+      color: {
+          // ['003366', '336699', '3366cc', '003399', '000099', '0000cc', '000066'],
+          // ['006666', '006699', '0099cc', '0066cc', '0033cc', '0000ff', '3333ff', '333399'],
+          // ['669999', '009999', '33cccc', '00ccff', '0099ff', '0066ff', '3366ff', '3333cc', '666699'],
+          // ['339966', '00cc99', '00ffcc', '00ffff', '33ccff', '3399ff', '6699ff', '6666ff', '6600ff', '6600cc'],
+          // ['339933', '00cc66', '00ff99', '66ffcc', '66ffff', '66ccff', '99ccff', '9999ff', '9966ff', '9933ff', '9900ff'],
+          // ['006600', '00cc00', '00ff00', '66ff99', '99ffcc', 'ccffff', 'ccccff', 'cc99ff', 'cc66ff', 'cc33ff', 'cc00ff', '9900cc'],
+          // ['003300', '009933', '33cc33', '66ff66', '99ff99', 'ccffcc', 'ffffff', 'ffccff', 'ff99ff', 'ff66ff', 'ff00ff', 'cc00cc', '660066'],
+          // ['333300', '009900', '66ff33', '99ff66', 'ccff99', 'ffffcc', 'ffcccc', 'ff99cc', 'ff66cc', 'ff33cc', 'cc0099', '993399'],
+          // ['336600', '669900', '99ff33', 'ccff66', 'ffff99', 'ffcc99', 'ff9999', 'ff6699', 'ff3399', 'cc3399', '990099'],
+          // ['666633', '99cc00', 'ccff33', 'ffff66', 'ffcc66', 'ff9966', 'ff6666', 'ff0066', 'd60094', '993366'],
+          // ['a58800', 'cccc00', 'ffff00', 'ffcc00', 'ff9933', 'ff6600', 'ff0033', 'cc0066', '660033'],
+          // ['996633', 'cc9900', 'ff9900', 'cc6600', 'ff3300', 'ff0000', 'cc0000', '990033'],
+          // ['663300', '996600', 'cc3300', '993300', '990000', '800000', '993333']
+      }
     }
   },
   methods: {
@@ -38,9 +37,37 @@ export default {
       localStorage.setItem('globalInfo', JSON.stringify(store.getters.getGlobalInfo))
       this.$emit('isclose')
     }
+    // showColor () {
+    //   alert(JSON.stringify(this.color))
+    // }
+  },
+  computed: {
+    getColor () {
+      // alert(this.color.length)
+      let color = []
+      let hex = ['00', '33', '66', '99', 'cc', 'ff']
+      let c = ''
+      let n = 0
+      for (let i = 0; i < 6; i++) {
+        n++
+        color[n] = []
+        for (let j = 0; j < 6; j++) {
+          for (let k = 0; k < 6; k++) {
+            c = hex[j] + hex[k] + hex[i]
+            // this.color[n] = []
+            // this.color[n].push(color)
+            // console.log(n)
+            color[n].push(c)
+          }
+        }
+      }
+      return color
+    }
   },
   mounted () {
-    // alert(this.color.length)
+    // this.$nextTick(() => {
+
+    // })
   }
 }
 </script>
@@ -48,13 +75,13 @@ export default {
 @import '../../../common/stylus/global.styl'
   .colorpicker
     position:relative
-    width:260px
+    width:720px
     height:260px
     margin: 0 auto
     .colorlist
       display:flex
       justify-content:center
-      width:260px
+      width:720px
       height:20px
       margin: 0 auto
       .color-detail
