431666954b2d8ef40e60dc92766ad42816f20aaf
daiwei <185098535@qq.com>
2017-08-14 23:55:18 +0800

    开始研究在线音乐api   好为music版块作铺垫

--- src/components/music/music.vue
+++ src/components/music/music.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="music">
+  	<div class="music_bg"></div>
+  	<div class="music_content"></div>
   </div>
 </template>
 <script>
+  import fecth from './../../utils/fecth.js'
   export default {
+  	methods: {
+  		searchMusic () {
+  			const apiUrl = 'http://www.daiwei.org/vue/server/music.php?inAjax=1&do=searchMusic'
+  			const data = {
+  				word: '周杰伦',
+  				limit: 10
+  			}
+  			fecth.post(apiUrl, data).then((res) => {
+  				console.log(JSON.stringify(res))
+  			}, (err) => {
+  				console.log(err)
+  			})
+  		}
+  	},
+  	mounted () {
+  		this.searchMusic()
+  	}
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
