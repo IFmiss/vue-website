6b4299e00aad0c869c6940051901484c10c73aa8
ifmiss <185098535@qq.com>
2017-08-18 18:15:03 +0800

    添加loading组件

---
+++ src/components/common/loader/loader.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="loader" v-show="isshow" :style="{backgroundColor:loaderbackground}">
+    <div class="loader-content" :style="{backgroundColor:loaderwidth}">
+        <div class="loader-range" :style="{borderYopColor:topbottomcolor,borderBottomColor:topbottomcolor,borderRightColor:leftrightcolor,borderLeftColor:leftrightcolor,opacity:borderopacity,animationDuration:borderduration}"></div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+        loaderwidth: {
+            default: '40px'
+        },
+        loaderbackground: {
+            default: '#2A323B'
+        },
+        topbottomcolor: {
+            default: '#545a6a'
+        },
+        leftrightcolor: {
+            default: '#d4d4db'
+        },
+        borderopacity: {
+            default: '0.5'
+        },
+        borderduration: {
+            default: '0.8s'
+        },
+        isshow: {
+            default: true
+        }
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../../common/response.css'
+@import '../../../common/stylus/global.styl'
+@import '../../../common/stylus/style.styl'
+.loader
+    position:fixed
+    top:0
+    left:0
+    right:0
+    bottom:0
+    background:#2A323B
+    z-index:1111
+    .loader-content
+        width: 40px
+        height: 40px
+        position: absolute
+        top: 50%
+        left: 50%
+        right: 0
+        transform:translate3d(-50%,-50%,0)
+        .loader-range
+            display: block
+            position: relative
+            width: 100%
+            height: 100%
+            animation: rotate 0.8s linear infinite
+            -webkit-border-radius: 100%
+            -moz-border-radius: 100%
+            border-radius: 100%
+            border-top: 1px solid #545a6a
+            border-bottom: 1px solid #d4d4db
+            border-left: 1px solid #545a6a
+            border-right: 1px solid #d4d4db
+            opacity: 0.5
+            animation-name: load_cycle 
+            @-webkit-keyframes load_cycle {
+                0% {
+                    transform:rotateZ(-360deg);
+                }
+                100% {
+                    transform:rotateZ(0deg);
+                }
+            }
+            @keyframes load_cycle {
+                0% {
+                    transform:rotateZ(-360deg);
+                }
+                100% {
+                    transform:rotateZ(360deg);
+                }
+            }
+</style>
--- src/components/works/works.vue
+++ src/components/works/works.vue
@@ -2,6 +2,7 @@
   <div class="works">
   	<div class="div_back" @click="back"><i class="icon-back"></i></div>
   	<div class="works_content">
+  		<loader></loader>
   		<!-- <workList title="jQuery 实用的loading插件" linkdemo="http://www.jq22.com/yanshi14633" linkproject="https://github.com/IFmiss/loading"></workList>
   		<workList title="jQuery 无缝滚动插件 jqueryslider" linkdemo="http://www.jq22.com/yanshi14632" linkproject="https://github.com/IFmiss/jquery-slider"></workList>
   		<workList title="jQuery 选择弹窗菜单插件centermenu" linkdemo="http://www.jq22.com/yanshi14420" linkproject="https://github.com/IFmiss/centermenu"></workList>
@@ -19,6 +20,7 @@
 <script>
 // import store from '../../store'
 import workList from './workList/workList.vue'
+import loader from '../common/loader/loader.vue'
 export default {
 	data () {
 		return {
@@ -92,7 +94,8 @@ export default {
 		}
 	},
 	components: {
-		'workList': workList
+		'workList': workList,
+		'loader': loader
 	}
 }
 </script>
