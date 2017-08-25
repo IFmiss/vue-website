bd4102020a3c71c5a34e132c0b6738b970a2cb53
ifmiss <185098535@qq.com>
2017-08-25 11:16:34 +0800

    修改天气信息  封装成一个模块

--- src/components/common/content/content.vue
+++ src/components/common/content/content.vue
@@ -49,32 +49,7 @@
       <div class="right-menu">
         <span class="place" :data-place="getPlace.region" @mouseover="showWeatherInfo" @mouseleave="hideWeatherInfo">
           {{getPlace.city}}
-          <transition name="fade-up">
-            <div class="weather-content" v-show="showWeatherList">
-              <div class="bg-info" :style="{backgroundColor:getGlobalInfo.contentInfo.bgcolor,opacity:getGlobalInfo.contentInfo.opacity}"></div>
-              <div class="weather-detail" v-if="getWeather">
-                <div class="weather-today" v-if="getWeather[0]">
-                  <div class="top-today">
-                    <span class="left-info">
-                      今天
-                    </span>
-                    <span class="right-info" :title="getWeather[0].date">
-                      {{getWeather[0].date}}
-                    </span>
-                  </div>
-                  <span class="type-today" :title="getWeather[0].type">{{getWeather[0].type}}</span>
-                  <span class="wendu-today" :title="getRightWendu(getWeather[0].low, getWeather[0].high)">{{getRightWendu(getWeather[0].low, getWeather[0].high)}}</span>
-                  <span class="wind-today" :title="getWeather[0].fx + ' ' + getWeather[0].fl">{{getWeather[0].fx}} {{getWeather[0].fl}}</span>
-                  <span class="notice-today" :title="getWeather[0].notice">{{getWeather[0].notice}}</span>
-                </div>
-                <div class="weather-other" v-if="getWeather" v-for="(item, index) in getWeather" v-show="index > 0 && index < 4">
-                  <span class="weather-time">{{index === 1 ? '明天:' : (index === 2 ? '后天:' : '大后天:')}}</span>
-                  <span class="weather-type" :title="item.type">{{item.type}}</span>
-                  <span class="weather-wendu" :title="getRightWendu(item.low, item.high)">{{getRightWendu(item.low, item.high)}}</span>
-                </div>
-              </div>
-            </div>
-          </transition>
+          <weather :isShow="showWeatherList"></weather>
         </span>
         <router-link tag="a" to="/setting" class="a-icon">
           <li class="li-icon" @click="hideLeftContent"><i class="icon-setting"></i></li>
@@ -90,6 +65,7 @@
 </template>
 <script>
 import store from './../../../store'
+import weather from './../weather/weather.vue'
 let t
 export default {
   data () {
@@ -116,10 +92,10 @@ export default {
       t = setTimeout(function () {
           _that.showWeatherList = false
       }, 800)
-    },
-    getRightWendu (l, h) {
-      return l.split(' ')[l.split(' ').length - 1] + ' ~ ' + h.split(' ')[l.split(' ').length - 1]
     }
+    // getRightWendu (l, h) {
+    //   return l.split(' ')[l.split(' ').length - 1] + ' ~ ' + h.split(' ')[l.split(' ').length - 1]
+    // }
   },
   computed: {
     getGlobalInfo () {
@@ -128,10 +104,13 @@ export default {
     getPlace () {
       // alert(JSON.stringify(store.getters.getPlace))
       return store.getters.getPlace
-    },
-    getWeather () {
-      return store.getters.getWeatherInfo
     }
+    // getWeather () {
+    //   return store.getters.getWeatherInfo
+    // }
+  },
+  components: {
+    weather
   }
 }
 </script>
@@ -186,74 +165,74 @@ export default {
           vertical-align:top
           cursor:pointer
           position:relative
-          .weather-content
-            border-radius:4px
-            position:absolute
-            height:auto
-            right:0
-            width:280px
-            overflow:hidden
-            &.fade-up-enter-to,&.fade-up-leave-to
-              transition:all 0.5s 0.2s
-            &.fade-up-enter,&.fade-up-leave-to
-              opacity:0
-              transform:translate3d(0,50px,0)
-            .bg-info
-              position:absolute
-              top:0
-              left:0
-              right:0
-              bottom:0
-              z-index:-1
-            .weather-detail
-              width:100%
-              height:100%
-              padding:15px
-              box-sizing:border-box
-              .weather-today
-                width:100%
-                height:auto
-                padding-bottom:5px
-                border-bottom:1px solid $border_bottom_color
-                .top-today
-                  width:100%
-                  height:50px
-                  font-size:0
-                  .left-info
-                    display:inline-block
-                    width:100px
-                    font-size:28px
-                  .right-info
-                    display:inline-block
-                    width:auto;
-                    font-size:14px
-                span
-                  display:block
-                  margin:0
-                  padding:4px 0
-                  font-size:12px
-                  height:auto
-                  line-height:1.5 
-              .weather-other
-                width:100%;
-                overflow:hidden
-                height:40px
-                border-bottom:1px dashed $border_bottom_color
-                padding:4px 0
-                font-size:0
-                .weather-wendu,.weather-type,.weather-time
-                  display:inline-block
-                  height:100%
-                  overflow:hidden
-                  text-overflow:ellipsis
-                  white-space:nowrap
-                  font-size:12px
-                .weather-wendu
-                  width:40%
-                .weather-time
-                  width:20%
-                .weather-type
-                  width:40%
+          // .weather-content
+          //   border-radius:4px
+          //   position:absolute
+          //   height:auto
+          //   right:0
+          //   width:280px
+          //   overflow:hidden
+          //   &.fade-up-enter-to,&.fade-up-leave-to
+          //     transition:all 0.5s 0.2s
+          //   &.fade-up-enter,&.fade-up-leave-to
+          //     opacity:0
+          //     transform:translate3d(0,50px,0)
+          //   .bg-info
+          //     position:absolute
+          //     top:0
+          //     left:0
+          //     right:0
+          //     bottom:0
+          //     z-index:-1
+          //   .weather-detail
+          //     width:100%
+          //     height:100%
+          //     padding:15px
+          //     box-sizing:border-box
+          //     .weather-today
+          //       width:100%
+          //       height:auto
+          //       padding-bottom:5px
+          //       border-bottom:1px solid $border_bottom_color
+          //       .top-today
+          //         width:100%
+          //         height:50px
+          //         font-size:0
+          //         .left-info
+          //           display:inline-block
+          //           width:100px
+          //           font-size:28px
+          //         .right-info
+          //           display:inline-block
+          //           width:auto;
+          //           font-size:14px
+          //       span
+          //         display:block
+          //         margin:0
+          //         padding:4px 0
+          //         font-size:12px
+          //         height:auto
+          //         line-height:1.5 
+          //     .weather-other
+          //       width:100%;
+          //       overflow:hidden
+          //       height:40px
+          //       border-bottom:1px dashed $border_bottom_color
+          //       padding:4px 0
+          //       font-size:0
+          //       .weather-wendu,.weather-type,.weather-time
+          //         display:inline-block
+          //         height:100%
+          //         overflow:hidden
+          //         text-overflow:ellipsis
+          //         white-space:nowrap
+          //         font-size:12px
+          //       .weather-wendu
+          //         width:40%
+          //       .weather-time
+          //         width:20%
+          //       .weather-type
+          //         width:40%
         a
           list-style:none
           display:inline-block
