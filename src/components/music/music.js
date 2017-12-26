8f373fc85a2e625ae0bb76260aa71d8b2765534b
daiwei <185098535@qq.com>
2017-12-26 22:17:11 +0800

    更新vue-message插件的使用   toast -> msg

--- src/components/music/music.js
+++ src/components/music/music.js
@@ -201,7 +201,7 @@ const musicApi = {
         fecth.get(apiUrl).then((res) => {
             // 如果代码不允许被播放（付费音乐）
             if (res.data.data[0].url === null) {
-                that.$toast('音乐无法播放,以为您切换到下一曲!')
+                that.$msg('音乐无法播放,以为您切换到下一曲!')
                 let initIndex = 0
                 const currentMusic = {
                     index: store.getters.getCurrentAudio.index ? store.getters.getCurrentAudio.index + 1 : initIndex + 1
