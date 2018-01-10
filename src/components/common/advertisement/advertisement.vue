6d313cc90d2dce21be11c5f70dd41bc7e5fe8ea3
daiwei <185098535@qq.com>
2018-01-10 21:24:22 +0800

    添加通用js内容 addclass  hasclass方法 以及adv的显示

---
+++ src/components/common/advertisement/advertisement.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="advertisement" v-if="!navigatorinfo.mobile">
+		<img src="http://oiq8j9er1.bkt.clouddn.com/zhifubaocode.png" title="扫码领红包，只是测试" alt="扫码领红包，只是测试">
+	</div>
+</template>
+
+<script>
+	import {Navigator} from '@/common/js/Navigator.js'
+	export default{
+		data () {
+			return {
+				navigatorinfo: Navigator.deviceVersion()
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/global.styl'
+	.advertisement
+		position: absolute
+		top: 50%
+		transform: translate(0, -50%)
+		right: 30px
+		width: 210px
+		height: auto
+		// background: $mask_color_opacity
+		img
+			width:100%
+			display:block
+</style>
