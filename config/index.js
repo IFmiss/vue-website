ef463abafe75171224ca6ac07609fa261ef9506a
ifmiss <185098535@qq.com>
2018-04-09 15:42:35 +0800

    gitpages的demo页

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     // productionSourceMap: true,
     assetsRoot: path.resolve(__dirname, '../demo'),
     assetsSubDirectory: '',
-    assetsPublicPath: 'https://ifmiss.github.io/vue-website/',
+    assetsPublicPath: 'https://ifmiss.github.io/vue-website/demo/',
     productionSourceMap: true,
 
     // Gzip off by default as many popular static hosts such as
