d7bb4a226e02fc01347070796139e4887cb5e05f
ifmiss <185098535@qq.com>
2018-04-09 15:17:08 +0800

    gitpages的demo页

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     // assetsPublicPath: 'new/',
     // productionSourceMap: true,
     assetsRoot: path.resolve(__dirname, '../demo'),
-    assetsSubDirectory: '',
+    assetsSubDirectory: 'vue-website/demo',
     assetsPublicPath: '/',
     productionSourceMap: true,
 
