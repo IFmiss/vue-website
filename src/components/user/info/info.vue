b878278bd13283de85a9a76173c81c8324c7455d
ifmiss <185098535@qq.com>
2018-05-02 23:31:12 +0800

    我的个人信息页面的撰写

--- src/components/user/info/info.vue
+++ src/components/user/info/info.vue
@@ -1,7 +1,38 @@
 <template>
 	<div class="user-info">
-		<p class="desc">敬请期待...</p>
-		<span class="loginout" @click="loginout">退出登录</span>
+		<!-- <p class="desc">敬请期待...</p>
+		<span class="loginout" @click="loginout">退出登录</span> -->
+		<div class="info-content">
+			<div class="user-basic-info">
+				<div class="user-avatar">
+					<img src="" alt="user-name">
+					<span class="change-avatar">更换图片</span>
+				</div>
+				<div class="user-basic">
+					<h3 class="user-nickname">这是用户的nickname</h3>
+					<p class="user-disc">这是用户的描述描述描述</p>
+					<!-- <div class=""></div> -->
+				</div>
+			</div>
+			<div class="mode-music">
+				
+			</div>
+			<div class="user-detail-info">
+				<!-- 用户的具体信息。音乐收藏。评价 点赞 互相评论的功能 -->
+				<ul class="detail-title">
+					<li class="title-list">音乐收藏</li>
+					<li class="title-list">我的建议</li>
+				</ul>
+				<div class="detail-content">
+					<div class="content-info music-list">
+						
+					</div>
+					<div class="content-info suggest-info">
+						
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
