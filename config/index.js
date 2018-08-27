b6bd7c516251b27e65f2c059d9054dac06b45b7e
zhangxiao <zx@166zx.com>
2018-08-27 16:47:09 +0800

    Update index.js
    打包后的就不要用sourceMap了

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'new',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: false,
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
